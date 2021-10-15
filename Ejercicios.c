2f6bfc68532d9bf1743cda6dc0e806f5e99c9748
lesly-chaparro <89046801+lesly-chaparro@users.noreply.github.com>
2021-10-14 21:50:23 -0500

    x

--- Ejercicios.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void main(void) {
-	int piolin=0;
-	printf ("Mi variable tiene el valor de %i", piolin);
-}
---
+++ prueba.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*Lesly Chaparro
+Prueba 2
+14-10-2021  10:00 PM */
+void main(void){
+    int piolin = 0;
+    printf ("La variable piolin vale %i",piolin);
+}
