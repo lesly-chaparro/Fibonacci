b8b57fd3d975d738d4edb08cfae529fc57a9559b
lesly-chaparro <89046801+lesly-chaparro@users.noreply.github.com>
2021-10-14 22:03:59 -0500

    x2

---
+++ variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*Lesly Chaparro
+Prueba 2
+14-10-2021  10:00 PM */
+void main(void){
+    //tipo de dato (int=entero) identificador (piolin)= valor inicial (0)
+    int piolin = 0;
+    //Constante entera (const int) identificador (silvestre)
+    int silvestre =2;
+    printf ("La variable piolin vale %i",piolin);
+    printf("\nLa variable silvestre vale %i", silvestre);
+    
+    piolin = 100;
+    silvestre =200;
+    printf("\n\nLa variable piolin 2 vale %i", silvestre);
+    printf("\nLa variable silvestre 2 vale %i", silvestre);
+}
