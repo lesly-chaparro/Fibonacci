6ba60ff73d4ad3a9cdb5d8368065c4f19ed3a77a
lesly-chaparro <89046801+lesly-chaparro@users.noreply.github.com>
2021-10-14 21:25:26 -0500

    hola mundo prueba mil ocho mil

---
+++ Ejercicios.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+	int piolin = 0;//Declarando entero e inicializando variable
+	/*printf("Lo que va a imprimir", nombre de la variable)
+	%i= entero--> la variable en este caso es de piolín*/
+	printf("Mi variable tiene el valor de %i", piolin);
+
+}
