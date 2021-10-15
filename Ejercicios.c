d8762b140b628cad167cd9cc60195b9b9921e51f
lesly-chaparro <89046801+lesly-chaparro@users.noreply.github.com>
2021-10-14 21:36:13 -0500

    Hola mundo

--- Ejercicios.c
+++ Ejercicios.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-int main() {
-	int piolin = 0;//Declarando entero e inicializando variable
-	/*printf("Lo que va a imprimir", nombre de la variable)
-	%i= entero--> la variable en este caso es de piolín*/
-	printf("Mi variable tiene el valor de %i", piolin);
+#include <stdlib.h>
 
+int main() {
+	printf("Hello world \n");
+	system ("pause");
+	return 0;
 }
